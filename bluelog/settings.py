e4cb3c8da08c1c896e8e1233f5dbd6f312187eef
wendy__wu <wendy__wu@outlook.com>
2020-04-08 17:14:06 +0800

    add images

--- bluelog/settings.py
+++ bluelog/settings.py
@@ -60,7 +60,8 @@ class TestingConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', prefix + os.path.join(basedir, 'data.db'))
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', prefix + os.path.join(basedir, 'data.db'))
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:zxc1015zxc@localhost/blogdb'
 
 
 config = {
